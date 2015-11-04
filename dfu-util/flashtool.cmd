2e8bb99426f2d7cc8cb4f26097daa139a3f0ce1d
vke <vincent.kessler@quantec-networks.de>
2015-11-04 15:17:40 +0100

    add dfu-util v0.8

---
+++ dfu-util/flashtool.cmd
@@ -0,0 +1,34 @@
+
+@echo off
+
+REM read in version information
+set /p VERSION=<version.txt
+
+call include_windows.cmd
+
+tools\win32\wget.exe -q --spider %VERSION_CHECK_URL%
+
+if not %ERRORLEVEL% == 0 (
+    echo ******************************************************************************
+    echo ******************************************************************************
+    echo ******************************************************************************
+    echo ******************************************************************************
+    echo WARNING, THIS IS NOT THE LATEST VERSION!!!!
+    echo You have 10 seconds to abort this script!
+    echo Get the latest version at
+    echo %VERSION_UPDATE_URL%
+    echo ******************************************************************************
+    echo ******************************************************************************
+    echo ******************************************************************************
+    echo ******************************************************************************
+    @ping 127.0.0.1 -n 11 -w 1000 > null
+)
+
+dfu-util_win32\dfu-util-static.exe %DFUUTIL_COMMANDLINE%
+
+echo ****************************
+echo * F E R T I G !            *
+echo ****************************
+
+
+pause
---
+++ dfu-util/flashtool.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+VERSION=$(cat version.txt)
+
+source include_linux.sh
+
+wget -q --spider $VERSION_CHECK_URL
+
+if [ ! $? = "0" ]; then
+	echo "********************************************************************************"
+	echo "********************************************************************************"
+	echo "********************************************************************************"
+	echo "********************************************************************************"
+	echo "WARNING, THIS IS NOT THE LATEST VERSION!!!!"
+	echo "You have 10 seconds to abort this script!"
+	echo "Get the latest version at"
+	echo "$VERSION_UPDATE_URL"
+	echo "********************************************************************************"
+	echo "********************************************************************************"
+	echo "********************************************************************************"
+	echo "********************************************************************************"
+	sleep 10
+fi
+
+# ensure that openocd binary is executable which may depend on the way it has been extracted from archive
+chmod +x ./dfu-util_linux_amd64/dfu-util
+
+./dfu-util_linux_amd64/dfu-util $DFUUTIL_COMMANDLINE
+
+echo "****************************"
+echo "* F E R T I G !            *"
+echo "****************************"
+
+read -n1 -r -p "Press any key to continue..." key
